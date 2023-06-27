f882e33bb2ff9be6881678d2baaa2e7769a528c9
CamilaQ <camila.quiros.07@gmail.com>
2023-06-26 23:07:37 -0300

    cambio rama

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -32,35 +32,41 @@ router.get('/:cid', async (req, res) => {
 
 
 //Agregar un producto al carrito o aumentar la cantidad del mismo si ya esta en el carrito. Req.body debe ser igual a pid o sino se rompe el json
-router.post('/:cid/product/:pid', async (req, res) => {
-    const {cid} = req.params;
-    const {pid} = req.params;
+router.post("/:cid/product/:pid", async (req, res) => {
+    const { cid } = req.params;
+    const { pid } = req.params;
     const cart = await cartManager.getCart();
-    const {product} = req.body;
-    let quantity = 1;
-    const newProduct = {product, quantity};
+    const { quantity } = req.body;
+    const newProduct = { product: parseInt(pid), quantity };
+    console.log(newProduct);
     const cartId = cart.find((c) => c.id == cid);
-    if(cartId){
-        const productId = cartId.products.find((p) => p.product == pid);
-        if(productId){
-            const newArray = cartId.products.filter((search) => search.product != pid)
-            newProduct.quantity = newProduct.quantity + productId.
-            quantity;
-            newArray.push(newProduct);
-            cartId.products = newArray
-            promises.writeFile('./database/cart.json',JSON.stringify(cart, null, '\t'))
-            res.status(201).json(cartId.products);
-            console.log(newProduct)
-        } else {
-            cartId.products.push(newProduct);
-            promises.writeFile('./database/cart.json',JSON.stringify(cart, null, '\t'))
-            res.status(201).json(cartId.products);
-            console.log('dos')
-        }
-    }else {
-        res.status(404).send('Carrito no encontrado');
+    if (cartId) {
+      const productId = cartId.products.find((p) => p.product == pid);
+      if (productId) {
+        const newArray = cartId.products.filter(
+          (search) => search.product != pid
+        );
+        newProduct.quantity = newProduct.quantity + productId.quantity;
+        newArray.push(newProduct);
+        cartId.products = newArray;
+        promises.writeFile(
+          "./database/cart.json",
+          JSON.stringify(cart, null, "\t")
+        );
+        res.status(201).json(cartId.products);
+      } else {
+        cartId.products.push(newProduct);
+        promises.writeFile(
+          "./database/cart.json",
+          JSON.stringify(cart, null, "\t")
+        );
+        res.status(201).json(cartId.products);
+      }
+    } else {
+      res.status(404).send("Carrito no encontrado");
     }
-});
+  });
+
 
 
 export default router;
