c6d956827dc41a8ab3d4c6a4488e2cb746ae4085
CamilaQ <camila.quiros.07@gmail.com>
2023-09-18 10:01:22 -0300

    postman

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import productsRouter from './routes/products.routes.js';
 import cartRouter from './routes/cart.routes.js';
 import mongoose from 'mongoose';
 import { cartModel } from './models/carts.models.js';
-import { productModel } from './models/products.models.js';
 
 const app = express();
 const PORT = 8080
@@ -17,17 +16,6 @@ mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clus
         // await cartModel.create({})
         const cart = await cartModel.findOne({_id: "65044859b9ff5276a5a5e46d"}).populate('products.id_prod')
         console.log(JSON.stringify(cart))
-
-        // const param = req.params
-        // const query = {};
-
-        // const options = { 
-        //     page: 2, 
-        //     sort: {price:'asc'}
-        // };
-
-        // const paginado = await productModel.paginate(query, options);
-        // console.log(paginado)
     })
     .catch((error) => console.log("Error en conexion con MongoDB: ", error))
 
